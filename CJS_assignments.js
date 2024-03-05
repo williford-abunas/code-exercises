469b2d403a43090babfcc92c9d704f053fe57b80
williford-abunas <abunaswilliford@gmail.com>
2024-03-05 21:01:34 +1300

    JS Fundamentals Part 1

---
+++ CJS_assignments.js
@@ -0,0 +1,8 @@
+const country = 'Philippines'
+const continent = 'Asia'
+let population = 1000000
+const isIsland = true
+let language
+console.log(typeof country, typeof population, typeof isIsland, typeof language)
+
+language = 'Filipino'
