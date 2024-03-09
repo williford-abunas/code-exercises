f7f50c0b7355245478f1aa2b68469d8a806ac158
williford-abunas <abunaswilliford@gmail.com>
2024-03-09 16:05:59 +1300

    destructuring

--- CJS_assignments2.js
+++ CJS_assignments2.js
@@ -11,8 +11,8 @@ const ratings = [
   ['ratingsCount', 144584],
 ]
 
-const [[, rating], [, ratingsCount]] = ratings
-console.log(rating, ratingsCount)
+// const [[, rating], [, ratingsCount]] = ratings
+// console.log(rating, ratingsCount)
 
 const ratingStars = [63405, 1808]
 
@@ -27,3 +27,19 @@ const { keywords: tags } = books[0]
 console.log(tags)
 const { language, programmingLanguage = 'unknown' } = books[6]
 console.log(language, programmingLanguage)
+let bookTitle = 'unknown'
+let bookAuthor = 'unknown'
+
+;({ title: bookTitle, author: bookAuthor } = books[0])
+
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0]
+console.log(bookRating)
+
+const printBookInfo = function ({ title, author, year = 'year unknown' }) {
+  console.log(`${title} by ${author}, ${year}`)
+}
+printBookInfo(books[0])
